10eb9ece125eecc0cdde38804e7e70a40d80416a
Djibril FALL <djibzo90@gmail.com>
2024-12-01 15:46:39 +0000

    Integration template (2)

--- M1APP/App_Start/BundleConfig.cs
+++ M1APP/App_Start/BundleConfig.cs
@@ -17,14 +17,32 @@ namespace M1APP
             // Utilisez la version de développement de Modernizr pour développer et apprendre. Puis, lorsque vous êtes
             // prêt pour la production, utilisez l'outil de génération à l'adresse https://modernizr.com pour sélectionner uniquement les tests dont vous avez besoin.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                        "~/Scripts/modernizr-*"
+                       ));
 
             bundles.Add(new Bundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Scripts/bootstrap.js",
+                       "~/assets/vendor/apexcharts/apexcharts.min.js",
+                        "~/assets/vendor/bootstrap/js/bootstrap.bundle.min.js",
+                        "~/assets/vendor/chart.js/chart.umd.js",
+                        "~/assets/vendor/echarts/echarts.min.js",
+                        "~/assets/vendor/quill/quill.js",
+                        "~/assets/vendor/simple-datatables/simple-datatables.js",
+                       "~/assets/vendor/tinymce/tinymce.min.js",
+                       "~/assets/vendor/php-email-form/validate.js",
+                         "~/assets/js/main.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/assets/vendor/bootstrap/css/bootstrap.min.css",
+                      "~/assets/vendor/bootstrap-icons/bootstrap-icons.css",
+                     "~/assets/vendor/boxicons/css/boxicons.min.css",
+                     "~/assets/vendor/quill/quill.snow.css",
+                     "~/assets/vendor/quill/quill.bubble.css",
+                     "~/assets/vendor/remixicon/remixicon.css",
+                     "~/assets/vendor/simple-datatables/style.css",
+                     "~/assets/css/style.css"));
         }
     }
 }
