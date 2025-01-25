540c1fd620ac6b6737336beda061af839d2019f6
Ngoundje <ngoundjembaye@gmail.com>
2025-01-25 16:25:16 +0000

    Message décrivant les modifications

--- M1APP/Models/BdAgenceVoyageContext.cs
+++ M1APP/Models/BdAgenceVoyageContext.cs
@@ -9,16 +9,13 @@ namespace M1APP.Models
 
         public DbSet<Utilisateur> utilisateurs { get; set; }
 
-       // public DbSet<Admin> admins { get; set; }
-
-        //public DbSet<Client> clients { get; set; }
+        public DbSet<Admin> Admins { get; set; }
 
         public DbSet<Gestionnaire> gestionnaires { get; set; }
         public DbSet<Agence> agences { get; set; }
 
         public DbSet<Client> Clients { get; set; }
 
-        public System.Data.Entity.DbSet<M1APP.Models.Admin> Admins { get; set; }
         // public DbSet<Flotte> flottes { get; set; }
         // public DbSet<Offre> offres { get; set; }
         // public DbSet<Reservation> reservations { get; set; }
