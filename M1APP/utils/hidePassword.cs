0fbf61962c3678ae7a07f810c649d985c4d88767
papabn <pndiaye999@gmail.com>
2025-01-30 09:38:30 +0000

    fix breakcrumb home url and hide password functionnality

---
+++ M1APP/utils/hidePassword.cs
@@ -0,0 +1,19 @@
+﻿namespace M1APP.utils
+{
+    /*
+  public static class HidePassword : Une classe statique ne peut pas être instanciée.*@
+   •	public static string ToAsterisks(this string input) : C'est une méthode d'extension. 
+   Les méthodes d'extension permettent d'ajouter des méthodes à des types existants 
+   sans les modifier directement.
+   Le mot-clé this avant le premier paramètre indique que cette méthode est une méthode d'extension pour le type string.
+    */
+    public static class HidePassword
+    {
+        public static string ToAsterisks(this string input)
+        {
+            return new string('*', input.Length);
+        }
+    }
+
+   
+}
